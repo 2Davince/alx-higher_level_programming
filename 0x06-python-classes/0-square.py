8c33e870472cb9ab6301405ccd66f897f9cc6f06
2Davince <davincenalemba2@gmail.com>
2023-08-29 14:56:47 -0700

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-        class Square:
-        def __init__(self, side_length):
-        self.side_length = side_length
+"""Define a class Square."""
+
+
+class Square:
+    """square representation"""
+    pass
 
