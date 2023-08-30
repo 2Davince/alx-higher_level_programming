59640acee06542c9075a9cfa7e3e1dc45e25e817
2Davince <davincenalemba2@gmail.com>
2023-08-30 07:14:13 -0700

    Print and count integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0)
-    ret = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    return (ret)
+def safe_print_list(my_list=[], z=0)
 
+
+"""Print z elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        z (int): The number of elements of my_list to print.
+
+    Returns:number of printed elements.
+    """
+
+ret = 0
+for i in range(z):
+    try:
+        print("{}".format(my_list[i]), end="")
+        ret += 1
+    except IndexError:
+        break
+print("")
+return (ret)
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """Prints an integer using "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        bool: True if printing is successful, False if a TypeError or ValueError occurs.
+    """
     try:
         print("{:d}".format(value))
-        return (True)
+        return True
     except (TypeError, ValueError):
-        return (False)
+        return False
+
 
